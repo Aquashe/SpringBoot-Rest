ea5246602c75a0304d0f4d600dd61a06eeda3331
Aquashe <thomasukuttybenny2709@gmail.com>
2025-01-13 22:44:00 +0530

    Post maon CRUD operation completed

--- Spring-boot-rest/src/main/java/com/thomas/Spring_boot_rest/JobRestController.java
+++ Spring-boot-rest/src/main/java/com/thomas/Spring_boot_rest/JobRestController.java
@@ -3,13 +3,13 @@ package com.thomas.Spring_boot_rest;
 import com.thomas.Spring_boot_rest.model.JobPost;
 import com.thomas.Spring_boot_rest.service.JobService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.awt.*;
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:3000")
 public class JobRestController {
 
     @Autowired
@@ -19,4 +19,36 @@ public class JobRestController {
     public List<JobPost> getAllJobs(){
         return  service.getAllJobs();
     }
+
+    @GetMapping(path = "jobPost/{postId}",produces = {"application/json"})
+    public JobPost getJob(@PathVariable("postId") int postId){
+        System.out.println("job with "+postId+" found");
+        return  service.getJob(postId);
+    }
+
+
+    @PostMapping(path = "jobPost",consumes = {"application/json"})
+    public JobPost addJob(@RequestBody JobPost jobPost){
+        System.out.println("job added");
+        service.addJob(jobPost);
+        return service.getJob(jobPost.getPostId());
+        //It better to return data from collection rather than from parameter itself
+    }
+
+
+    @PutMapping("jobPost")
+    public JobPost updateJob(@RequestBody JobPost jobPost){
+        System.out.println("job updated");
+        service.updateJob(jobPost);
+        return service.getJob(jobPost.getPostId());
+        //It better to return data from collection rather than from parameter itself
+    }
+
+    @DeleteMapping("jobPost/{postId}")
+    public String deletJob(@PathVariable("postId") int postId){
+        System.out.println("job with "+postId+" deleted");
+        service.deleteJob(postId);
+        return  "job with "+postId+" deleted";
+    }
+
 }
--- Spring-boot-rest/src/main/java/com/thomas/Spring_boot_rest/service/JobService.java
+++ Spring-boot-rest/src/main/java/com/thomas/Spring_boot_rest/service/JobService.java
@@ -30,6 +30,15 @@ public class JobService {
         return  repo.getAllJobs();
     }
 
+    public JobPost getJob(int postId){
+        return  repo.getJob(postId);
+    }
 
+    public void updateJob(JobPost jobPost) {
+        repo.updateJob(jobPost);
+    }
 
+    public void deleteJob(int postId) {
+        repo.deleteJob(postId);
+    }
 }
